459ffa8cd9c544bb3824d760302709d9a08636c6
jrpt1017 <jonneltolosa111@gmail.com>
2022-09-04 12:06:42 +0800

    Update mockData.js

--- src/mockData.js
+++ src/mockData.js
@@ -1,4 +1,42 @@
-[{
-
-  
-}]
+export const mockData = [
+  {
+    title: "Danali National Park and Preserve",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ex ut blandit condimentum.",
+  },
+  {
+    title: "Beautiful peaks of the Himalayas",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ex ut blandit condimentum.",
+  },
+  {
+    title: "Karakoram Range in Northern Pakistan",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ex ut blandit condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing.",
+  },
+  {
+    title: "Mount Kilimanjaro",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ex ut blandit condimentum.",
+  },
+  {
+    title: "Olympus Range of Greece",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ex ut blandit condimentum.",
+  },
+  {
+    title: "Mont Blanc",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ex ut blandit condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing.",
+  },
+  {
+    title: "The peaks of Patagonia",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ex ut blandit condimentum.",
+  },
+  {
+    title: "Dolomites of Italy",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ex ut blandit condimentum.",
+  },
+];
