f1ec9d4311ccbec1556560ede947e25401de4536
jrpt1017 <jonneltolosa111@gmail.com>
2022-09-04 11:17:49 +0800

    Initial implementation of skeletal pages

---
+++ src/components/MainComponent/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header";
+import MainContent from "../MainContent";
+
+const MainComponent = () => {
+  return (
+    <>
+      <Header />
+      <MainContent />
+    </>
+  );
+};
+
+export default MainComponent;
