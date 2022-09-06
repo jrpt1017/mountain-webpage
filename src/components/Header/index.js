c321fd33bc5fb397a5c93cd2ccd23d2f61ffd9cb
jrpt1017 <jonneltolosa111@gmail.com>
2022-09-07 00:21:43 +0800

    Finished implementation of Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,43 @@
 import React from "react";
 import "./index.scss";
 const Header = () => {
-  return <div className="header-wrapper">Header</div>;
+  return (
+    <div className="header-wrapper">
+      <img
+        className="adrenalin-title-container"
+        src="/adrenalin_title.svg"
+        alt="adrenalin-title"
+        align="left"
+      />
+      <div className="title-container">
+        <p className="header-title">Mountains</p>
+        <p className="header-subtitle">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+
+      <div className="triangle-container">
+        <img
+          className="violet-triangle"
+          src="/violet-triangle.svg"
+          alt="violet-triangle"
+          align="right"
+        />
+        <img
+          className="green-triangle"
+          src="/green-triangle.svg"
+          alt="green-triangle"
+          align="right"
+        />
+        <img
+          className="blue-triangle"
+          src="/blue-triangle.svg"
+          alt="blue-triangle"
+          align="right"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
