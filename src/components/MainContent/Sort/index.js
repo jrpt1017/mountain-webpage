c0e98dfc764b63abf7066056ef6bd02016246248
jrpt1017 <jonneltolosa111@gmail.com>
2022-09-08 14:42:44 +0800

    Implement mobile responsive for search bar and sort fields

--- src/components/MainContent/Sort/index.js
+++ src/components/MainContent/Sort/index.js
@@ -3,13 +3,13 @@ import "./index.scss";
 
 const Sort = ({ options, handleSort, value }) => {
   return (
-    <>
+    <div style={{ height: "100%" }}>
       <select class="form-control" onChange={handleSort}>
         {options.map((optionItem) => {
           return <option value={optionItem}>{optionItem}</option>;
         })}
       </select>
-    </>
+    </div>
   );
 };
 
