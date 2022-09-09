ed0ae3b36afe35ab65311720c1b16df1b208cd4e
jrpt1017 <jonneltolosa111@gmail.com>
2022-09-09 16:53:26 +0800

    Implement search functionality

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -8,27 +8,7 @@ const MainContent = () => {
   const filterOptions = ["Sort a A-Z", "Sort a Z-A"];
   const [cards, setCards] = useState(mockData);
   const [searchTerm, setsearchTerm] = useState("");
-
-  const handleSortItems = (e) => {
-    const sortValue = e.target.value;
-    const cardCopy = [...cards];
-    if (sortValue === filterOptions[0]) {
-      cardCopy.sort((firstItem, secondItem) => {
-        return firstItem.title.localeCompare(secondItem.title);
-      });
-    } else {
-      cardCopy
-        .sort((firstItem, secondItem) => {
-          return firstItem.title.localeCompare(secondItem.title);
-        })
-        .reverse();
-    }
-    setCards(cardCopy);
-  };
-
-  const handleSearchMountain = (e) => {
-    setsearchTerm(e.target.value);
-  };
+  const [filterValue, setFilterValue] = useState(filterOptions[0]);
 
   // Upon component is mounted, sort it alphabetically -- by default.
   useEffect(() => {
@@ -39,13 +19,53 @@ const MainContent = () => {
     setCards(cardCopy);
   }, []);
 
-  useEffect(() => {
-    const cardsCopy = [...cards];
-    const items = cardsCopy.filter((item) => {
-      return item.title.toLowerCase().includes(searchTerm.toLocaleLowerCase());
+  const sortToAZ = (cardCopy) => {
+    cardCopy.sort((firstItem, secondItem) => {
+      return firstItem.title.localeCompare(secondItem.title);
     });
-    console.log(items.map((i) => i.title));
-  }, [searchTerm]);
+  };
+
+  const sortToZA = (cardCopy) => {
+    cardCopy
+      .sort((firstItem, secondItem) => {
+        return firstItem.title.localeCompare(secondItem.title);
+      })
+      .reverse();
+  };
+
+  const handleSortItems = (e) => {
+    const sortValue = e.target.value;
+    setFilterValue(sortValue);
+    const cardCopy = [...cards];
+    if (sortValue === filterOptions[0]) {
+      sortToAZ(cardCopy);
+    } else {
+      sortToZA(cardCopy);
+    }
+    setCards(cardCopy);
+  };
+
+  const handleSearchMountain = (e) => {
+    const term = e.target.value;
+    setsearchTerm(term);
+
+    if (term !== "") {
+      const items = cards.filter((item) => {
+        return item.title
+          .toLowerCase()
+          .includes(searchTerm.toLocaleLowerCase());
+      });
+      setCards(items);
+    } else {
+      const originalData = mockData;
+      if (filterValue === filterOptions[0]) {
+        sortToAZ(originalData);
+      } else {
+        sortToZA(originalData);
+      }
+      setCards(originalData);
+    }
+  };
 
   return (
     <div className="main-content-wrapper">
